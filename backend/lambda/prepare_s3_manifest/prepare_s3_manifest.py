6c9a2ae1da8313f602d44334748dfcd29ccf1845
Aloysius <aloysiusng5@gmail.com>
2023-11-12 01:25:40 +0800

    update missing handler

--- backend/lambda/prepare_s3_manifest/prepare_s3_manifest.py
+++ backend/lambda/prepare_s3_manifest/prepare_s3_manifest.py
@@ -9,30 +9,36 @@ import boto3
 s3 = boto3.client("s3")
 bucket_name = os.environ["S3_BUCKET_NAME"]
 
-now = datetime.now()
-year = now.year
-month = now.month
-day = now.day
+def lambda_handler(event, context):
+    now = datetime.now()
+    year = now.year
+    month = now.month
+    day = now.day
 
-# builder for manifest file
-manifest = defaultdict(list)
-URIPrefixes = [
-    f"s3://{bucket_name}/output/reddit_overall_sentiments/{year}/{month}/{day}/",
-    f"s3://{bucket_name}/output/reddit_comprehended_posts/{year}/{month}/{day}/",
-    f"s3://{bucket_name}/output/reddit_comprehended_comments/{year}/{month}/{day}/",
-]
-manifest["fileLocations"] = [URIPrefixes]
-manifest["globalUploadSettings"] = {
-    "format": "CSV",
-    "delimiter": ",",
-    "containsHeader": "true",
-}
+    # builder for manifest file
+    manifest = defaultdict(list)
+    URIPrefixes = [
+        f"s3://{bucket_name}/output/reddit_overall_sentiments/{year}/{month}/{day}/",
+        f"s3://{bucket_name}/output/reddit_comprehended_posts/{year}/{month}/{day}/",
+        f"s3://{bucket_name}/output/reddit_comprehended_comments/{year}/{month}/{day}/",
+    ]
+    manifest["fileLocations"] = [URIPrefixes]
+    manifest["globalUploadSettings"] = {
+        "format": "CSV",
+        "delimiter": ",",
+        "containsHeader": "true",
+    }
 
-print(json.dumps(manifest, indent=4))
+    print(json.dumps(manifest, indent=4))
 
-# write manifest file to S3
-s3.put_object(
-    Bucket=bucket_name,
-    Key="manifests/reddit_manifest.json",
-    Body=json.dumps(manifest, indent=4),
-)
+    # write manifest file to S3
+    try:
+        s3.put_object(
+            Bucket=bucket_name,
+            Key="manifests/reddit_manifest.json",
+            Body=json.dumps(manifest, indent=4),
+        )
+    except Exception as e:
+        print(e)
+        return {"statusCode": 500, "body": "S3 manifest generation failed " + str(e)}
+    return {"statusCode": 200, "body": "S3 manifest generation success"}
